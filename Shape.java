f8cc8f80bd5e49dddc61f1a4f17c702790626a77
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-22 21:14:44 +0600

    create an constructor.

--- Shape.java
+++ Shape.java
@@ -2,3 +2,10 @@ interface Shape{
     double calculateArea();
 }
 
+class Circle implements Shape{
+    private double radius;
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+}
