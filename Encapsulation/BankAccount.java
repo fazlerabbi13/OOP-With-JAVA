2dddf5bcec4df4f03211a938c2fc906f6d43cc04
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-23 00:11:57 +0600

    Public getter and setter methods (controlled access).

---
+++ Encapsulation/BankAccount.java
@@ -0,0 +1,36 @@
+package Encapsulation;
+
+public class BankAccount {
+    private long accountNo;
+    private String name;
+    private String email;
+    private float amount;
+
+    public long getaccountNo(){
+        return accountNo;
+    }
+    public void setAccountNo(long accountNo){
+        this.accountNo=accountNo;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public float getAmount(){
+        return amount;
+    }
+    public void setAmount(float amount){
+        this.amount=amount;
+    }
+}
