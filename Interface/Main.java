8bf6bfd1ab60e912ea867a9e1aff4c31feb238ba
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-24 22:24:52 +0600

    Calculator class implementing Add and Sub.

--- Interface/Main.java
+++ Interface/Main.java
@@ -9,19 +9,19 @@ interface Sub{
 }
 
 // Calculator class implementing Add and Sub 
-// class Cal implements Add , Sub
-// {
-//   	// Method to add two numbers
-//   	public int add(int a,int b){
-//       	return a+b;
-//     }
+class Cal implements Add , Sub
+{
+  	// Method to add two numbers
+  	public int add(int a,int b){
+      	return a+b;
+    }
   
-//   	// Method to sub two numbers
-//   	public int sub(int a,int b){
-//     	return a-b;
-//     }
+  	// Method to sub two numbers
+  	public int sub(int a,int b){
+    	return a-b;
+    }
   
-// }
+}
 
 
 public class Main {
