f5a0256c82e8d6682a5179e6160f8fa5db28d844
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-25 23:30:38 +0600

    nested interface is created.

---
+++ Interface/Nested.java
@@ -0,0 +1,24 @@
+interface Parent {
+    interface Test {
+        void show();
+    }
+}
+
+// class Child implements Parent.Test {
+//     public void show() {
+//         System.out.println("show method of interface");
+//     }
+// }
+
+
+// Main Class
+class Nested
+{
+    public static void main(String[] args)
+    {
+        Parent.Test obj;
+        Child t = new Child();
+        obj = t;
+        obj.show();
+    }
+}
