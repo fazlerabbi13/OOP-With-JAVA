f11102091c6fbc2b5bff96a4cd5c7b0fc7987a48
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-17 18:32:12 +0600

    create an constructor and pass a parameter.

---
+++ Constructor/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    int x;
+
+    public Main(int y){
+        x=y;
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main(6);
+        System.out.println(obj.x);
+    }
+}
