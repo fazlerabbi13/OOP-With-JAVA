70b86627cf3c22656ade1c4a2e03ac1994455ca8
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-17 18:15:49 +0600

    modify attribute.

---
+++ ClassAttributes/ModifyAttribute.java
@@ -0,0 +1,10 @@
+
+public class ModifyAttribute {
+    int x;
+
+    public static void main(String args[]){
+        ModifyAttribute obj = new ModifyAttribute();
+        obj.x = 50;
+        System.out.println(obj.x);
+    }
+}
